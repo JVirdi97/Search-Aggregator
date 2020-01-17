320922320c8575f448efcded2ca9d97612fd2b4f
JVirdi97 <55207647+JVirdi97@users.noreply.github.com>
2020-01-17 15:15:48 +0000

    update to search aggregator
    Updated the file so it is in compliance of PEP 8

--- SearchAggregator2.py
+++ SearchAggregator2.py
@@ -1,56 +1,68 @@
-import bs4, requests, tkinter
+import bs4
+import requests
+import tkinter
 from bs4 import BeautifulSoup
-from tkinter import * #from tkinter import everything
+from tkinter import *  # import * <=> import everything
+from selenium import webdriver
 
-def Google_Search():
-    SearchTerm=SearchBar.get() #get the search term from the search bar in the form
-    SearchBar.delete(0,"end") #empty the search bar (maybe keep the search query?)
-    response=requests.get("https://www.google.com/search?q="+SearchTerm) #gets the first results page of the search
-    if(response.status_code == requests.codes.ok):  #check the status of the request, if it failed, show what code it is
+
+def dogooglesearch(): # define the fn here or the program will break
+    searchterm = SearchBar.get()
+    text.delete("0.0", END)
+    if searchterm == "": # if user doesnt enter a search term
+        text.insert(INSERT, """Please enter a search term
+""")
+        return # get out of the function if there's nothing to search for
+        # get the search term from the search bar in the form
+    response = requests.get("https://www.google.com/search?q="+SearchTerm)
+    # gets the first results page of the search
+    if response.status_code == requests.codes.ok:
+        """check the status of the request, if it failed, show what code
+        the response is """ 
         print("Google request successful")
     else:
-        print("Google request failed, the response code is: "+response.status_code)
-    soup=BeautifulSoup(response.text, "html.parser") #turns the html page into a soup, to extract information from
-    for link in soup.find_all("a"): #foreach link in the page html
-        if("/url?q=" in str(link)): #if the html line contains url then keep and process it
-            link_href=str(link.get("href")) #get link to the page
-            cutoff=link_href.find("&sa=U&ved") #get rid of useless end piece
-            Site_Link=link_href[7:cutoff] #get rid of useless bit at start
-            text.insert(INSERT, Site_Link) #put the link in the text box
+        print("Google request failed, the response code is: "
+              + response.status_code)
+    soup = BeautifulSoup(response.text, "html.parser")
+    # turns the html page into a soup, to extract information from
+    for link in soup.find_all("a"):  # foreach link in the page html
+        if "/url?q=" in str(link):  # if the html line contains url then keep and process it
+            link_href = str(link.get("href"))  # get link to the page
+            cutoff = link_href.find("&sa=U&ved")  # get rid of useless end piece
+            site_link = link_href[7:cutoff]  # get rid of useless bit at start
+            text.insert(INSERT, Site_Link)  # put the link in the text box
             text.insert(INSERT, """"
 
-""") #line break
-"""the 2 text.insert lines insert the link into the text box, and then put a
-clear line between the links.
+""")
+
+
+"""the 2 text.insert lines insert the link into the text box, and then
+put a clear line between the links.
 """
 
-#SearchTerm=input("Enter your search terms: ") #get the search terms from user
-#Page_html = Google_Search(SearchTerm) #get the page html
-#Google_link_Searcher(Page_html) #get the links to pages
-""" ^3 lines above are not needed, just allow me to test the google_Search fn
-without using the form """
-Form=Tk() #registers form as the fn which will be called from the TK mainloop
-Form.geometry("800x500") #make the form [x,y] in size
+Form = Tk()  # registers form as the fn which will be called from the TK mainloop
+Form.geometry("800x500")  # make the form [x,y] in size
 
-#canvas
-canvas=Canvas(Form, width=400,height=300) # canvas with dimensions [400,300]
-canvas.pack() # add the canvas to the form, Form
+# canvas
+canvas = Canvas(Form, width=400,height=300)  # canvas with dimensions [400,300]
+canvas.pack()  # add the canvas to the form, Form
 
-#button to search with
-SearchBtn=Button(Form, text="Search", command=Google_Search)
-#^ when the user clicks the button, it calls the google_search function to get the search results and print them
-canvas.create_window(350,140,window=SearchBtn)
-#^add the SearchBtn to the canvas in the form
+# button to search with
+SearchBtn = Button(Form, text="Search", command=doGoogleSearch)
+"""^ when the user clicks the button, it calls the google_search
+    function to get the search results and print them """
+canvas.create_window(350,140,window=SearchBtn)  # ^add the SearchBtn to the canvas in the form
 
-#SearchBar for people to enter what they want to search
-SearchBar= Entry(Form, width=40)
-canvas.create_window(200,140,window=SearchBar)
-"""^ adds the search bar to the canvas. this is used to input the search query
-for the Google_Search function """
+# SearchBar for people to enter what they want to search
+SearchBar = Entry(Form, width=40)
+canvas.create_window(200, 140, window=SearchBar)
+"""^ adds the search bar to the canvas. this is used to input the
+    search query for the Google_Search function """
 
-#text to print the links to
-text=Text(Form)
+# text to print the links to
+text = Text(Form)
 text.pack()
-#^the contents of the text are manipulated elsewhere using the text.insert(INSERT, "[insert thing here]") function
+"""^the contents of the text are manipulated elsewhere using the
+    text.insert(INSERT, "[insert thing here]") function """
 
-Form.mainloop() #to draw the form to process the events on the form
+Form.mainloop()  # to draw the form and to process the events on the form
